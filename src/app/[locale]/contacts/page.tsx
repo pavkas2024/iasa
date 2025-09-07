b2c83ce16dd80c320263a8f43b54dcc76baec678
pavkas2024 <site.kasyanov@gmail.com>
2025-09-07 19:01:33 +0300

    update

--- src/app/[locale]/contacts/page.tsx
+++ src/app/[locale]/contacts/page.tsx
@@ -5,16 +5,17 @@ import type { Contact } from "@/types/contacts";
 
 type Locale = "uk" | "en";
 
-interface PageProps {
-  params: { locale: Locale };
-}
-
 export const metadata = {
   title: "Контакти ІПСА | Contacts of the IASA",
-  description: "Контактна інформація Інституту прикладного системного аналізу. Contact information of the Institute of Applied System Analysis.",
+  description:
+    "Контактна інформація Інституту прикладного системного аналізу. Contact information of the Institute of Applied System Analysis.",
 };
 
-export default async function Contacts({ params }: PageProps) {
+export default async function Contacts({
+  params,
+}: {
+  params: { locale: Locale };
+}) {
   const { locale } = params;
   const data = await getContactsData();
   const contacts: Contact[] = data.contacts;
